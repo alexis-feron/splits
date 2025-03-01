581a27d08a5d06c1312d58ad113e3cf137563209
Alexis Feron <alexisferon081@gmail.com>
2025-03-01 18:06:45 +0100

    🔍 Improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import Navbar from "./components/Navbar";
+import favicon from "./favicon.ico";
 import "./globals.css";
 
 export const metadata = {
-  title: "Splits",
+  title: "Splits - F1 Standings, Game Information, and Upcoming Events",
   description:
-    "Your one-stop destination for F1 standings, game, and upcoming events.",
+    "Splits is your one-stop destination for comprehensive F1 standings, detailed game information, and updates on upcoming Formula 1 events. Stay informed with the latest in the world of Formula 1.",
+  keywords:
+    "F1, Formula 1, standings, game information, upcoming events, Formula 1 news, F1 updates",
+  author: "Alexis Feron",
 };
+
 export default function RootLayout({
   children,
 }: {
@@ -14,6 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta charSet="utf-8" />
+        <meta property="og:image" content={favicon.src} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </head>
       <body className="bg-white text-stone-900 flex flex-col min-h-screen">
         {/* Header */}
         <Navbar />
@@ -30,6 +46,7 @@ export default function RootLayout({
               className="hover:text-black"
               href="https://alexis-feron.com"
               target="_blank"
+              title="Alexis Feron Portfolio"
               rel="noopener noreferrer"
             >
               Alexis Feron
